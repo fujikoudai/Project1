a7ca5fb980e20fabbf424b473f564b68724988f3
煌大 藤林 <fujibayasi.kodai@gmail.com>
2025-04-23 16:05:15 +0900

    端数が出ないようにする

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -22,20 +22,20 @@ int main() {
 	int y = uniform_int_distribution<>(1, 20)(rand);
 	questions[0].q = to_string(x) + "*"+ to_string(y);
 	questions[0].a = x * y;
-
+	//割り算
 	x = uniform_int_distribution<>(1, 30)(rand);
 	y = uniform_int_distribution<>(1, 20)(rand);
-	questions[1].q = to_string(x) + "/" + to_string(y);
-	questions[1].a = x / y;
+	questions[1].q = to_string(x*y) + "/" + to_string(y);
+	questions[1].a = x ;
 	//複雑な式 
 	x= uniform_int_distribution<>(1,100 )(rand);
 	y = uniform_int_distribution<>(1,10)(rand);
 	int z= uniform_int_distribution<>(1, 10)(rand);
 	int w = uniform_int_distribution<>(1,10)(rand);
 	questions[2].q =
-		to_string(x) + "-(" + to_string(y) + "+" + to_string(z) + ") % " +
+		to_string(x) + "-(" + to_string(y*w) + "+" + to_string(z*w) + ") % " +
 		to_string(w);
-	questions[2].a = x - (y + z) / w;
+	questions[2].a = x - (y + z) ;
 	cout << "[リクルート対策クイズ]\n";
 	
 	for (const auto& e : questions) {
